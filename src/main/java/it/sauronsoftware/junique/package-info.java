3f98869722361e3591adf6d32a5a89443f402138
Paulo Borges <poolborges@hotmail.com>
2022-12-05 21:50:00 +0000

    Add missing License header to java file

--- src/main/java/it/sauronsoftware/junique/package-info.java
+++ src/main/java/it/sauronsoftware/junique/package-info.java
@@ -1,7 +1,19 @@
-/**
- * Provides the classes necessary to implements a user related cross-JVM exclusive lock concept. 
+/*
+ * JUnique - Helps in preventing multiple instances of the same application
  * 
- * <p>
+ * Copyright (C) 2008-2010 Carlo Pelliccia (www.sauronsoftware.it)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version
+ * 2.1, as published by the Free Software Foundation.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License 2.1 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License version 2.1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 package it.sauronsoftware.junique;
